07dca4c05666affa9dbfd7b2c2e061522778a236
Leonardo <rignanese.leo@gmail.com>
2018-05-27 17:18:24 +0200

    Cleaning the project with gitignore (part2)

---
+++ DirittoMigrantiAPI/DirittoMigrantiAPI/Models/TokenRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DirittoMigrantiAPI.Models
+{
+    public class TokenRequest
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
