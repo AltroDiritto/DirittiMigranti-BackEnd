3a74f87dea7f6790422f7671dc002cdcb77b78ed
Leonardo <rignanese.leo@gmail.com>
2018-05-27 17:05:20 +0200

    Using ASP.NET Core
    - Login with token generation
    - Token validator in some requests

---
+++ DirittoMigrantiAPI/DirittoMigrantiAPI/Models/User.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+namespace DirittoMigrantiAPI.Models
+
+{
+    //https://msdn.microsoft.com/en-us/library/system.componentmodel.dataannotations(v=vs.110).aspx
+    //https://docs.microsoft.com/it-it/aspnet/core/tutorials/first-mvc-app/validation?view=aspnetcore-2.0
+
+    //[Bind(Exclude = "Id")]
+    public class User
+    {
+        // Id used as a key in the dictionary where all the users are stored
+        public long Id { get; set; }
+
+        [StringLength(30, MinimumLength = 5)]
+        [Required]
+        public string Username { get; set; }
+
+        [StringLength(40, MinimumLength = 8)]
+        [Required]
+        public string Password { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        public string Surname { get; set; }
+    }
+}
---
+++ DirittoMigrantiAPI/DirittoMigrantiAPI/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using DirittoMigrantiAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DirittoMigrantiAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Usa un db interno
+            services.AddDbContext<UserContext>(options =>
+                options.UseInMemoryDatabase("UserList"));
+            //options.ValidateScopes = false
+
+
+
+            //Configurare il middleware di autenticazione di ASP.NET Core per supportare i token JWT
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+
+                    //Importante: indicare lo stesso Issuer, Audience e chiave segreta
+                    //usati anche nel JwtTokenMiddleware
+                    ValidIssuer = "Issuer",
+                    ValidAudience = "Audience",
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(Program.key)
+                  ),
+                    //Tolleranza sulla data di scadenza del token
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            app.UseMiddleware<JwtTokenMiddleware>();
+            app.UseAuthentication();
+
+
+            //DEBUG
+            var context = serviceProvider.GetRequiredService<UserContext>();
+            AddTestData(context);
+
+            app.UseMvc();
+        }
+
+        private static void AddTestData(UserContext context)
+        {
+            var testUser1 = new User
+            {
+                Username = "utente1",
+                Password = "utente1"
+            };
+            context.Users.Add(testUser1);
+
+            var testUser2 = new User
+            {
+                Username = "utente2",
+                Password = "utente2"
+            };
+            context.Users.Add(testUser2);
+
+            context.SaveChanges();
+        }
+    }
+}
