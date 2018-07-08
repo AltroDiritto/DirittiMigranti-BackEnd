3ce8b36a90d9fb9ce6f0968d1b35510c9e9e7ac3
Leonardo <dev.rignaneseleo@gmail.com>
2018-07-08 22:58:06 +0200

    Fix NewNews

--- DirittoMigrantiAPI/DirittoMigrantiAPI/API/ContentApi.cs
+++ DirittoMigrantiAPI/DirittoMigrantiAPI/API/ContentApi.cs
@@ -19,18 +19,42 @@ namespace DirittoMigrantiAPI.API{    [Route("api/cont")]    public class Cont
         [HttpGet("getN/{contentId}", Name = "GetNews")]
         public IActionResult GetNewsAPI(long contentId)        {            News news = base.GetNews(contentId);            if (news == null)                return NotFound();            return Ok(news);        }        [Authorize(Roles = "Consultant")]
         [HttpPost("newN", Name = "NewNews")]        public IActionResult CreateNewsAPI([FromBody] News news)        {            var userId = (User as ClaimsPrincipal)?.FindFirstValue(ClaimTypes.SerialNumber);
-            if (string.IsNullOrEmpty(userId)) return BadRequest();// TODO aggiungere ovunque
+            if (string.IsNullOrEmpty(userId)) return BadRequest();
 
-            news.CreationDate = DateTime.Now;            news.Writer = (Consultant)Startup.users[1];//TODO da sistemare!!!!!!!!!!!!!!!!!!!!
+            news.CreationDate = DateTime.Now;            User user = context.Users.Find(long.Parse(userId));
+            if (user == null) return BadRequest();
+
+            //TODO IWriter
+            news.Writer = (Consultant)user; 
 
             if (!ModelState.IsValid) return BadRequest();
 
-            News checkNews = NewNews(news);            if (checkNews == null) return BadRequest();            context.SaveChanges();            return Ok(checkNews);        }        [Authorize(Roles = "Consultant")]        [HttpDelete("delN/{contentId}", Name = "DeleteNews")]        public IActionResult DeleteNewsAPI(long contentId)        {
+            News newNews = NewNews(news);            if (newNews == null) return BadRequest();            context.SaveChanges();            return Ok(newNews);        }        [Authorize(Roles = "Consultant")]        [HttpDelete("delN/{contentId}", Name = "DeleteNews")]        public IActionResult DeleteNewsAPI(long contentId)        {
             var ris = DeleteNews(contentId);            context.SaveChanges();            return Ok(ris);        }
 
         //[Authorize(Roles = "Consultant")]
         [HttpPost("setN/{newsId}/{isPublished}", Name = "SetNews")]//TODO ricevere parametri
         public IActionResult SetNewsStateAPI(long newsId, bool isPublished)        {            return Ok(SetNewsState(newsId, isPublished));        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         #endregion
         #region PRACTICE        [HttpGet("getPuP/{contentId}", Name = "GetPublicPractice")]
         public IActionResult GetPublicPracticeAPI(long contentId)        {            Practice practice = base.GetPublicPractice(contentId);            if (practice == null)                return NotFound();            return Ok(practice);        }        [Authorize(Roles = "Consultant, Operator")]
